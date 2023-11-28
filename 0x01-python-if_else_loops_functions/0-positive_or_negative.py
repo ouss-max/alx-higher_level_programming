18cb2bdab246315966dd3101f6cefd0a980d136c
ouss-max <mamoto10ichiro@gmail.com>
2023-11-28 17:13:14 +0100

    signal checker

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print("{:d} is positive".format(number))
+elif number < :
+    print("{:d} is negative".format(number))
+else:
+    print("{d:} is null".format(number))
