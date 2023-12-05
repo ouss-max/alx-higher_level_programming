34d8e355994e0cdc6d2301f62865142c99ad7023
ouss-max <mamoto10ichiro@gmail.com>
2023-12-06 00:56:52 +0100

    delete.py fix

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 
-def delete_at(my_list=[], idx=0):
+/usr/bin/bash: line 1: q: command not found
 
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-        
+
     else:
         del my_list[idx]
     return my_list
+
+def delete_at(my_list=[], idx=0):
+   
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+        
+    for i in range(len(my_list)):
+        if i == idx:
+            my_list = my_list[:i] + my_list[i+1:]
+            break
+
+    return my_list 
