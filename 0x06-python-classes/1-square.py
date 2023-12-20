4109e9be387cb9f670569adf15cf5a2b9d06bf45
ouss-max <mamoto10ichiro@gmail.com>
2023-12-20 02:06:15 +0100

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Define Square, the type is a class"""
+
+
+class Square:
+
+    def __init__(self, size):
+        """Initializing of the new square"""
+        self.__size = size
